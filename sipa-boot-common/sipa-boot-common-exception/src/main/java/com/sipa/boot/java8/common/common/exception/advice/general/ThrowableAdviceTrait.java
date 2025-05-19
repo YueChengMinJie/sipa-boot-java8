2f91b16998ea0ef4fb096c5710845a201ce67b02
caszhou <zhouxiajie@qq.com>
2025-05-19 23:27:50 +0800

    feat: runtime exception 错误处理.

--- sipa-boot-common/sipa-boot-common-exception/src/main/java/com/sipa/boot/java8/common/common/exception/advice/general/ThrowableAdviceTrait.java
+++ sipa-boot-common/sipa-boot-common-exception/src/main/java/com/sipa/boot/java8/common/common/exception/advice/general/ThrowableAdviceTrait.java
@@ -8,9 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.sipa.boot.java8.common.archs.error.ErrorEntity;
 import com.sipa.boot.java8.common.archs.error.UnknownErrorEntity;
 import com.sipa.boot.java8.common.common.exception.advice.ExceptionAdviceTrait;
 import com.sipa.boot.java8.common.exceptions.ApplicationException;
+import com.sipa.boot.java8.common.exceptions.BadRequestException;
 
 /**
  * @author caszhou
@@ -31,17 +33,20 @@ public interface ThrowableAdviceTrait extends ExceptionAdviceTrait {
     @ExceptionHandler(Throwable.class)
     default Object handleThrowable(final HttpServletRequest request, final HttpServletResponse response,
         final Throwable exception) {
-        String errorMessage = ApplicationException.DEFAULT_APPLICATION_EXCEPTION_ERROR_MESSAGE
-            + (getExceptionHandlingProperties().isShowUnknownExceptionStack()
-                ? "\n\n" + ExceptionUtils.getStackTrace(exception) : "");
-
-        UnknownErrorEntity errorEntity = new UnknownErrorEntity(errorMessage);
-
-        /*
-         * Writing error stacktrace to logs with tracking id next to it
-         */
-        LOGGER.error("Unhandled error during request processing (tracking id: [{}]):", errorEntity.getTrackingId(),
-            exception);
+        ErrorEntity errorEntity;
+        String errorMessage;
+        if (exception instanceof RuntimeException) {
+            errorMessage = exception.getMessage();
+            errorEntity = new ErrorEntity(new BadRequestException(errorMessage));
+        } else {
+            errorMessage = ApplicationException.DEFAULT_APPLICATION_EXCEPTION_ERROR_MESSAGE
+                + (getExceptionHandlingProperties().isShowUnknownExceptionStack()
+                    ? "\n\n" + ExceptionUtils.getStackTrace(exception) : "");
+            UnknownErrorEntity unknownErrorEntity = new UnknownErrorEntity(errorMessage);
+            errorEntity = unknownErrorEntity;
+            LOGGER.error("Unhandled error during request processing (tracking id: [{}]):",
+                unknownErrorEntity.getTrackingId(), exception);
+        }
 
         return handle(request, response, ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorEntity));
     }
